9106eb8919b5258d9d7b001e302be65bbe96d652
Marvin-LOUIS <marvinlouis@idea-box.tech>
2017-10-17 16:53:18 +0200

    backend cookie

--- coockie.js
+++ coockie.js
@@ -1,37 +1,64 @@
+const infla = 5;
 var image = document.getElementById("image");
 var multiTwo = document.getElementById("multiTwo");
 var multiFive = document.getElementById("multiFive");
 var reduxTwo = document.getElementById("reduxTwo");
 var reduxFive = document.getElementById("reduxFive");
 var scoreHtml = document.getElementById("scoreHtml");
+var prix = [50,200,50,200];
 var score = 0;
-var multiplicateur = 1;
-var reducteur = 1;
+var multiClic = 1;
+var multiAuto = 1;
 
-function addScoreClick(){
-  score += multiplicateur;
+multiTwo.innerHTML = "Clique X2 (" + prix[0] + ")";
+multiFive.innerHTML = "Clique X5 (" + prix[1] + ")";
+reduxTwo.innerHTML = "Clique X2 (" + prix[2] + ")";
+reduxFive.innerHTML = "Clique X5 (" + prix[3] + ")";
+
+function addScoreClick() {
+  score += multiClic;
   scoreHtml.innerHTML= score;
 }
 
-function addScoreAuto(){
-  score += reducteur;
+function addScoreAuto() {
+  score += multiAuto;
   scoreHtml.innerHTML= score;
 }
 
 function addMultiTwo() {
-  multiplicateur *= 2;
+  if (score >= prix[0]) {
+    multiClic *= 2;
+    score -= prix[0];
+    prix[0] *= infla;
+    multiTwo.innerHTML = "Clique X2 (" + prix[0] + ")";
+  }
 }
 
 function addMultiFive() {
-  multiplicateur *= 5;
+  if (score >= prix[1]) {
+    multiClic *= 5;
+    score -= prix[1];
+    prix[1] *= infla;
+    multiFive.innerHTML = "Clique X5 (" + prix[1] + ")";
+  }
 }
 
 function addReduxTwo() {
-  reducteur *= 2;
+  if (score >= prix[2]) {
+    multiAuto *= 2;
+    score -= prix[2];
+    prix[2] *= infla;
+    reduxTwo.innerHTML = "Clique X2 (" + prix[2] + ")";
+  }
 }
 
 function addReduxFive() {
-  reducteur *= 5;
+  if (score >= prix[3]) {
+    multiAuto *= 5;
+    score -= prix[3];
+    prix[3] *= infla;
+    reduxFive.innerHTML = "Clique X5 (" + prix[3] + ")";
+  }
 }
 
 image.addEventListener("click",addScoreClick);
