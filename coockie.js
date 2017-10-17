26db63abfc7c1646c28b4f4f4aaeb35a5542c8f0
Valerian Thomas <thomasvalerian93@gmail.com>
2017-10-17 15:41:34 +0200

    adding score times 2, need to create buying composent

--- coockie.js
+++ coockie.js
@@ -1,20 +1,35 @@
-
 var image = document.getElementById("image");
 
 var score = 0;
+var timeMe = 1;
+var clickMe=1;
+
 var scoreHtml = document.getElementById("scoreHtml");
+var multi = document.querySelector(".me");
 
 
-image.addEventListener("click",function(){
-   
-    
-    score = score +1;
-    
-    scoreHtml.innerHTML= score;
+image.addEventListener("click", function() {
 
-    
+
+  score = score + (1*clickMe);
+
+  scoreHtml.innerHTML = score;
+
+
 });
 
+multi.addEventListener("click", function() {
+  timeMe = timeMe + 2;
+  clickMe= clickMe+2
 
-    
-    
+});
+
+var yolo = function() {
+  score = score + (1*timeMe);
+
+  scoreHtml.innerHTML = score;
+
+
+}
+
+var scoreN = setInterval(yolo, 1000);
