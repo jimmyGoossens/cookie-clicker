99a8420f5965d3c0c224962eb0fc23ab65635755
Valerian Thomas <thomasvalerian93@gmail.com>
2017-10-18 14:31:35 +0200

    final shop done

--- coockie.js
+++ coockie.js
@@ -1,19 +1,23 @@
-const infla = 5;
+
 var image = document.getElementById("image");
 var multiTwo = document.getElementById("multiTwo");
+var multiTwoNbr=0;
 var multiFive = document.getElementById("multiFive");
+var multiFiveNbr=0;
 var reduxTwo = document.getElementById("reduxTwo");
+var reduxTwoNbr=0;
 var reduxFive = document.getElementById("reduxFive");
+var reduxFiveNbr=0;
 var scoreHtml = document.getElementById("scoreHtml");
 var prix = [50,200,50,200];
 var score = 0;
 var multiClic = 1;
 var multiAuto = 1;
 
-multiTwo.innerHTML = "Clique X2 (" + prix[0] + ")";
-multiFive.innerHTML = "Clique X5 (" + prix[1] + ")";
-reduxTwo.innerHTML = "Clique X2 (" + prix[2] + ")";
-reduxFive.innerHTML = "Clique X5 (" + prix[3] + ")";
+multiTwo.innerHTML = "Clique X2 (" + prix[0] + ")"+" achat:"+multiTwoNbr;
+multiFive.innerHTML = "Clique X5 (" + prix[1] + ")"+" achat:"+multiFiveNbr;
+reduxTwo.innerHTML = "Clique X2 (" + prix[2] + ")"+" achat:"+reduxTwoNbr;
+reduxFive.innerHTML = "Clique X5 (" + prix[3] + ")"+" achat:"+reduxFiveNbr;
 
 function addScoreClick() {
   score += multiClic;
@@ -29,8 +33,9 @@ function addMultiTwo() {
   if (score >= prix[0]) {
     multiClic *= 2;
     score -= prix[0];
-    prix[0] *= infla;
-    multiTwo.innerHTML = "Clique X2 (" + prix[0] + ")";
+    prix[0] *= 5;
+    multiTwoNbr+=1;
+    multiTwo.innerHTML = "Clique X2 (" + prix[0] + ")" +" achat:"+multiTwoNbr;
   }
 }
 
@@ -38,8 +43,9 @@ function addMultiFive() {
   if (score >= prix[1]) {
     multiClic *= 5;
     score -= prix[1];
-    prix[1] *= infla;
-    multiFive.innerHTML = "Clique X5 (" + prix[1] + ")";
+    prix[1] *= 10;
+    multiFiveNbr+=1;
+    multiFive.innerHTML = "Clique X5 (" + prix[1] + ")"+" achat:"+multiFiveNbr;
   }
 }
 
@@ -47,8 +53,9 @@ function addReduxTwo() {
   if (score >= prix[2]) {
     multiAuto *= 2;
     score -= prix[2];
-    prix[2] *= infla;
-    reduxTwo.innerHTML = "Clique X2 (" + prix[2] + ")";
+    prix[2] *= 5;
+    reduxTwoNbr+=1;
+    reduxTwo.innerHTML = "Clique X2 (" + prix[2] + ")"+" achat:"+reduxTwoNbr;
   }
 }
 
@@ -56,8 +63,9 @@ function addReduxFive() {
   if (score >= prix[3]) {
     multiAuto *= 5;
     score -= prix[3];
-    prix[3] *= infla;
-    reduxFive.innerHTML = "Clique X5 (" + prix[3] + ")";
+    prix[3] *= 10;
+    reduxFiveNbr+=1;
+    reduxFive.innerHTML = "Clique X5 (" + prix[3] + ")"+" achat:"+reduxFiveNbr;
   }
 }
 
